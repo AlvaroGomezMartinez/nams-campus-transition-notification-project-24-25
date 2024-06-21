21822f292eb8ec45d3b1bffde9d225d8a472146f
Alvaro Gomez <alvaro.gomez2011@gmail.com>
2024-06-20 21:00:34 -0500

    Last updates

--- Code.js
+++ Code.js
@@ -1,77 +1,22 @@
-// To learn how to use this script, refer to the documentation:
-// https://developers.google.com/apps-script/samples/automations/mail-merge
+const EMAIL_SENT_COL = "Transition Notice email date";
 
-/*
-Copyright 2022 Martin Hawksey
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    https://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
- 
-/**
- * @OnlyCurrentDoc
-*/
- 
-/**
- * Change these to match the column names you are using for email 
- * recipient addresses and email sent column.
-*/
-const RECIPIENT_COL  = "Recipient";
-const EMAIL_SENT_COL = "Date Campus Email Sent";
- 
-/** 
- * Creates the menu item "Notify Campuses" for user to run scripts on drop-down.
- */
 function onOpen() {
   const ui = SpreadsheetApp.getUi();
   ui.createMenu('Notify Campuses')
-      .addItem('Send Emails', 'sendEmails')
-      .addToUi();
+    .addItem('Send emails to students with blanks in column G', 'sendEmails')
+    .addToUi();
 }
- 
-/**
- * Sends emails from sheet data.
- * @param {string} subjectLine (optional) for the email draft message
- * @param {Sheet} sheet to read data from
-*/
-function sendEmails(subjectLine = "AEP Placement Transition Plan", sheet=SpreadsheetApp.getActiveSheet()) {
 
-  // Skipped the browser prompt below and just set the subject line
-  // if (!subjectLine){
-  //   subjectLine = Browser.inputBox("Send Campus Emails", 
-  //                                     "Type or copy/paste the subject line of the Gmail " +
-  //                                     "draft message you would like to mail merge with:",
-  //                                     Browser.Buttons.OK_CANCEL);
-                                      
-  //   if (subjectLine === "cancel" || subjectLine == ""){ 
-  //   // If no subject line, finishes up
-  //   return;
-  //   }
+function sendEmails(sheet=SpreadsheetApp.openById('1qTdUYWNZ5plMB6FlVYH-QTZkJajXaJBnovzrWxmCGac').getSheetByName('Return to HC 24-25')) { //{ .getActiveSheet()) {
 
-  // Gets the draft Gmail message to use as a template
-  const emailTemplate = getGmailTemplateFromDrafts_(subjectLine);
-  
-  // Gets the data from the passed sheet
-  const dataRange = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Return to HC 23-24').getDataRange();
+  const dataRange = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Return to HC 24-25').getDataRange();
   const data = dataRange.getDisplayValues();
-  const heads = data.shift(); 
-  
-  // Gets the index of the column named 'Email Status' (Assumes header names are unique)
-  // @see http://ramblings.mcpher.com/Home/excelquirks/gooscript/arrayfunctions
+  const heads = data.shift();
+
+  // Gets the index of the column named 'Transition Notice email date' (Assumes header names are unique)
   const emailSentColIdx = heads.indexOf(EMAIL_SENT_COL);
   
   // Converts 2d array into an object array
-  // See https://stackoverflow.com/a/22917499/1027723
-  // For a pretty version, see https://mashe.hawksey.info/?p=17869/#comment-184945
   const obj = data.map(r => (heads.reduce((o, k, i) => (o[k] = r[i] || '', o), {})));
 
   // Creates an array to record sent emails
@@ -80,45 +25,30 @@ function sendEmails(subjectLine = "AEP Placement Transition Plan", sheet=Spreads
   // Loops through all the rows of data
   obj.forEach(function(row, rowIdx){
     // Only sends emails if email_sent cell is blank and not hidden by a filter
-    if (row[EMAIL_SENT_COL] == ''){
+    if (row[EMAIL_SENT_COL] === ''){
       try {
         const campusInfo = getInfoByCampus(row['Campus']);
         const recipients = campusInfo.recipients;
         const driveLink = campusInfo.driveLink;
+        const emailTemplate = getGmailTemplateFromDrafts_(row, driveLink);
 
         const msgObj = fillInTemplateFromObject_(emailTemplate.message, row, driveLink);
 
-        // See https://developers.google.com/apps-script/reference/gmail/gmail-app#sendEmail(String,String,String,Object)
-        // If you need to send emails with unicode/emoji characters change GmailApp for MailApp
-        // Uncomment advanced parameters as needed (see docs for limitations)
+        // Uncomment advanced parameters as needed
         GmailApp.sendEmail(recipients, msgObj.subject, msgObj.text, {
-          // htmlBody: msgObj.html,
-          // bcc: 'a.bcc@email.com',
-          // cc: 'a.cc@email.com',
-          // from: 'an.alias@email.com',
-          // name: 'name of the sender',
-          // replyTo: 'a.reply@email.com',
-          // noReply: true, // if the email should be sent from a generic no-reply email address (not available to gmail.com users)
-          // attachments: emailTemplate.attachments,
-          // inlineImages: emailTemplate.inlineImages
           htmlBody: msgObj.html,
+          replyTo: 'john.decker@nisd.net',
         });
         // Edits cell to record email sent date
         out.push([new Date()]);
       } catch(e) {
-        // modify cell to record error
         out.push([e.message]);
       }
     } else {
       out.push([row[EMAIL_SENT_COL]]);
     }
   });
-  
-  /**
-     * Determine recipients based on the value in the "Campus" column.
-     * @param {string} campusValue value in the "Campus" column
-     * @return {object} containing a comma-separated list of recipients and Google Drive Folder link
-     */
+
     function getInfoByCampus(campusValue) {
       switch (campusValue.toLowerCase()) {
         // case 'bernal':
@@ -249,99 +179,55 @@ function sendEmails(subjectLine = "AEP Placement Transition Plan", sheet=Spreads
         case 'test':
           return {
             recipients: 'alvaro.gomez@nisd.net', //, john.decker@nisd.net, sheila.yeager@nisd.net',
-            driveLink: '1gBcGRl700LGfhMPrHx32dZ2j3MwPlqYS'
+            driveLink: '1I7mkmBa3-sO_eG6f2KlSKyUg-G71ZAYK'
           };
         default:
-          return '';
+          return {
+            recipients: '',
+            driveLink: ''
+          };
       }
     }
 
   // Updates the sheet with new data
   sheet.getRange(2, emailSentColIdx+1, out.length).setValues(out);
-  
-  /**
-   * Get a Gmail draft message by matching the subject line.
-   * @param {string} subject_line to search for draft message
-   * @return {object} containing the subject, plain and html message body and attachments
-  */
-  function getGmailTemplateFromDrafts_(subject_line){
-    try {
-      // get drafts
-      const drafts = GmailApp.getDrafts();
-      // filter the drafts that match subject line
-      const draft = drafts.filter(subjectFilter_(subject_line))[0];
-      // get the message object
-      const msg = draft.getMessage();
 
-      // Handles inline images and attachments so they can be included in the merge
-      // Based on https://stackoverflow.com/a/65813881/1027723
-      // Gets all attachments and inline image attachments
-      const allInlineImages = draft.getMessage().getAttachments({includeInlineImages: true,includeAttachments:false});
-      const attachments = draft.getMessage().getAttachments({includeInlineImages: false});
-      const htmlBody = msg.getBody(); 
+  function getGmailTemplateFromDrafts_(row, driveLink) {
+    return {
+      message: {
+        subject: 'AEP Placement Transition Plan',
+        text: 'Hardcoded body text',
+        html: `Dear ${row['Campus']},<br><br>
+              ${row['Student Name']} has nearly completed their assigned placement at NAMS and should be returning to ${row['Campus']} on or around ${row['Return Date']}.<br><br>   
+              On their last day of placement, they will be given withdrawal documents and the parents/guardians will have been called and told to contact ${row['Campus']} to set up an appointment to re-enroll and meet with an administrator/counselor.<br><br>  
+              Below are links and attachments to a Personalized Transition Plan (with notes from NAMS' assigned social worker), the student's AEP Transition Plan (with grades and notes from their teachers at NAMS), and a link to ${row['Campus']}'s folder with all of the transition plans for this year.<br><br>
+              Please let me know if you have any questions or concerns.<br><br>
+              Thank you for all you do,<br>
+              JD<br><br>
+              ${row['Merged Doc URL - Personalized Transition Plan']}<br>
+              ${row['Merged Doc URL - AEP Placement Transition Plan']}<br>
+              https://drive.google.com/drive/folders/${driveLink}<br>`,
+      }, 
+      attachments: [], 
+      inlineImages: {}
+    };
 
-      // Creates an inline image object with the image name as key 
-      // (can't rely on image index as array based on insert order)
-      const img_obj = allInlineImages.reduce((obj, i) => (obj[i.getName()] = i, obj) ,{});
-
-      //Regexp searches for all img string positions with cid
-      const imgexp = RegExp('<img.*?src="cid:(.*?)".*?alt="(.*?)"[^\>]+>', 'g');
-      const matches = [...htmlBody.matchAll(imgexp)];
-
-      //Initiates the allInlineImages object
-      const inlineImagesObj = {};
-      // built an inlineImagesObj from inline image matches
-      matches.forEach(match => inlineImagesObj[match[1]] = img_obj[match[2]]);
-
-      return {message: {subject: subject_line, text: msg.getPlainBody(), html:htmlBody}, 
-              attachments: attachments, inlineImages: inlineImagesObj };
-    } catch(e) {
-      throw new Error("Oops - can't find Gmail draft");
-    }
-
-    /**
-     * Filter draft objects with the matching subject linemessage by matching the subject line.
-     * @param {string} subject_line to search for draft message
-     * @return {object} GmailDraft object
-    */
-    function subjectFilter_(subject_line){
-      return function(element) {
-        if (element.getMessage().getSubject() === subject_line) {
-          return element;
-        }
-      }
-    }
   }
-  
-/**
-   * Fill template string with data object
-   * @see https://stackoverflow.com/a/378000/1027723
-   * @param {string} template string containing {{}} markers which are replaced with data
-   * @param {object} data object used to replace {{}} markers
-   * @return {object} message replaced with data
-  */
-  function fillInTemplateFromObject_(template, data, driveLink) {
+
+  function fillInTemplateFromObject_(template, row, driveLink) {
     // We have two templates one for plain text and the html body
     // Stringifing the object means we can do a global replace
     let template_string = JSON.stringify(template);
-
-
     // Token replacement
     template_string = template_string.replace(/{{[^{}]+}}/g, key => {
-      if (key === '{{DriveLink}}') {
+      if (key === '${driveLink}') {
       return escapeData_(driveLink);
       }
-      return escapeData_(data[key.replace(/[{}]+/g, "")] || "");
+      return escapeData_(row[key.replace(/[{}]+/g, "")] || "", driveLink);
     });
     return  JSON.parse(template_string);
   }
 
-  /**
-   * Escape cell data to make JSON safe
-   * @see https://stackoverflow.com/a/9204218/1027723
-   * @param {string} str to escape JSON special characters from
-   * @return {string} escaped string
-  */
   function escapeData_(str) {
     return str
       .replace(/[\\]/g, '\\\\')
